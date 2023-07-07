d67ef6e3e37436695c85191f8575fbb2e66da19c
dvillamar <dayilisvv2017@gmail.com>
2023-07-07 15:26:05 -0500

    prueba

--- src/estudiantes/dispositivos.module.ts
+++ src/estudiantes/dispositivos.module.ts
@@ -7,7 +7,7 @@ import { Dispositivo } from './entities/dispositivo.entity';
 @Module({
   providers: [DispositivosResolver, DispositivosService],
   imports:[
-    TypeOrmModule.forFeature([Dispositivo])
+    TypeOrmModule.forFeature([Dispositivo])   
   ]
 })
 export class DispositivosModule {}
